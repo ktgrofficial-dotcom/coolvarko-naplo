5edf94f5f01e6548739bddd79c9a59f75197449c
ktgrofficial-dotcom <ktgr.official@gmail.com>
2026-09-20 14:08:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "coolvarko-v33";
+const CACHE_NAME = "coolvarko-v34";
 const urlsToCache = ["/coolvarko-naplo/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
